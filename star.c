d5970fe400733f0acffa96cb59395b632c25832a
Param Singh <techhero724@gmail.com>
2025-01-07 16:54:13 +0530

    updating program

--- star.c
+++ star.c
@@ -1,4 +1,20 @@
 #include<stdio.h>
+
+int main(){
+    for(int i=5;i>=1;i--){
+        for(int j=0;j<=5-i;j++)
+            printf(" ");
+        for(int k = 1;k<2*i;k++)
+           printf("*");
+           printf("\n");
+    }
+    
+
+
+    return 0;
+}
+
+/* n#include<stdio.h>
 int main(){
     int i,j,k;
     for(i=5;i>=1;i--)
@@ -9,6 +25,5 @@ int main(){
         printf("*");
         printf("\n");
     }
-
     return 0;
-}
+} */
